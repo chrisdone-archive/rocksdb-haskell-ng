6497472d40e9a14e2e844c060ee3edb521d19e2e
Chris Done <chrisdone@gmail.com>
2018-01-22 10:43:59 +0000

    Add options and iterators

--- src/Database/RocksDB.hs
+++ src/Database/RocksDB.hs
@@ -6,21 +6,39 @@
 -- | A safe binding to RocksDB.
 
 module Database.RocksDB
-    -- * Functions
+    -- * Basic Functions
   ( open
-  , OpenConfig(..)
+  , defaultOptions
+  , Options(..)
+  , DB
+  , Compression(..)
   , close
   , put
+  , defaultWriteOptions
+  , WriteOptions(..)
   , get
-    -- * Types
-  , DBH
+  , defaultReadOptions
+  , ReadOptions(..)
+  , write
+  , BatchOp(..)
+  , delete
+  -- * Iterators
+  , createIter
+  , releaseIter
+  , iterSeek
+  , iterEntry
+  , iterNext
+  , Iterator
+  -- * Misc types
   , RocksDBException(..)
   ) where
 
 import           Control.Concurrent
 import           Control.Exception
 import           Control.Monad
+import           Control.Monad.IO.Class
 import           Data.ByteString (ByteString)
+import           Data.ByteString.Internal
 import qualified Data.ByteString.Unsafe as S
 import           Data.Coerce
 import           Data.Typeable
@@ -33,17 +51,33 @@ import           System.Directory
 --------------------------------------------------------------------------------
 -- Publicly-exposed types
 
-
 -- | Option options.
-data OpenConfig = OpenConfig
-  { openConfigCreateIfMissing :: !Bool
-  , openConfigFilePath :: !FilePath
+data Options = Options
+  { optionsCreateIfMissing :: !Bool
+  , optionsFilePath :: !FilePath
+  , optionsCompression :: !Compression
   }
+
+data Compression
+  = NoCompression
+  | SnappyCompression
+  | ZlibCompression
+  deriving (Eq, Show)
+
+data WriteOptions = WriteOptions {}
+
+data ReadOptions = ReadOptions {}
+
+-- | Batch operation
+data BatchOp
+  = Put !ByteString !ByteString
+  | Del !ByteString
+  deriving (Eq, Show)
 
 -- | A handle to a RocksDB database. When handle becomes out of reach,
 -- the database is closed.
-data DBH = DBH
-  { dbhVar :: !(MVar (Maybe (ForeignPtr DB)))
+data DB = DB
+  { dbVar :: !(MVar (Maybe (ForeignPtr CDB)))
   }
 
 -- | An exception thrown by this module.
@@ -54,6 +88,25 @@ data RocksDBException
   deriving (Typeable, Show)
 instance Exception RocksDBException
 
+data Iterator
+
+--------------------------------------------------------------------------------
+-- Defaults
+
+defaultOptions :: FilePath -> Options
+defaultOptions fp =
+  Options
+  { optionsCreateIfMissing = False
+  , optionsFilePath = fp
+  , optionsCompression = NoCompression
+  }
+
+defaultWriteOptions :: WriteOptions
+defaultWriteOptions = WriteOptions {}
+
+defaultReadOptions :: ReadOptions
+defaultReadOptions = ReadOptions {}
+
 --------------------------------------------------------------------------------
 -- Publicly-exposed functions
 
@@ -61,7 +114,7 @@ instance Exception RocksDBException
 --
 -- Throws 'RocksDBException' on failure.
 --
--- The database will be closed when the 'DBH' is garbage collected or
+-- The database will be closed when the 'DB' is garbage collected or
 -- when 'close' is called on it.
 --
 -- With LC_ALL=C, two things happen:
@@ -71,164 +124,248 @@ instance Exception RocksDBException
 -- So, we create the folder by ourselves, and for that we
 -- need to set the encoding we're going to use. On Linux
 -- it's almost always UTC-8.
-open :: OpenConfig -> IO DBH
-open config = do
-  withFilePath
-    (openConfigFilePath config)
-    (\pathPtr ->
-       withOptions
-         (\optsPtr -> do
-            c_rocksdb_options_set_create_if_missing
-              optsPtr
-              (openConfigCreateIfMissing config)
-            dbhPtr <-
-              bracket
-                (replaceEncoding config)
-                restoreEncoding
-                (const
-                   (do when
-                         (openConfigCreateIfMissing config)
-                         (createDirectoryIfMissing
-                            True
-                            (openConfigFilePath config))
-                       v <-
-                         assertNotError
-                           "c_rocksdb_open"
-                           (c_rocksdb_open optsPtr pathPtr)
-                       pure v))
-            dbhFptr <- newForeignPtr c_rocksdb_close_funptr dbhPtr
-            dbRef <- newMVar (Just dbhFptr)
-            pure (DBH {dbhVar = dbRef})))
+open :: MonadIO m => Options -> m DB
+open config =
+  liftIO
+    (do withFilePath
+          (optionsFilePath config)
+          (\pathPtr ->
+             withOptions
+               (\optsPtr -> do
+                  c_rocksdb_options_set_create_if_missing
+                    optsPtr
+                    (optionsCreateIfMissing config)
+                  dbhPtr <-
+                    bracket
+                      (replaceEncoding config)
+                      restoreEncoding
+                      (const
+                         (do when
+                               (optionsCreateIfMissing config)
+                               (createDirectoryIfMissing
+                                  True
+                                  (optionsFilePath config))
+                             v <-
+                               assertNotError
+                                 "c_rocksdb_open"
+                                 (c_rocksdb_open optsPtr pathPtr)
+                             pure v))
+                  dbhFptr <- newForeignPtr c_rocksdb_close_funptr dbhPtr
+                  dbRef <- newMVar (Just dbhFptr)
+                  pure (DB {dbVar = dbRef}))))
 
 -- | Close a database.
 --
 -- * Calling this function twice has no ill-effects.
 --
 -- * You don't have to call this; if you no longer hold a reference to
---   the @DBH@ then it will be closed upon garbage collection. But you
+--   the @DB@ then it will be closed upon garbage collection. But you
 --   can call this function to do it early.
 --
 -- * Further operations (get/put) will throw an exception on a closed
--- * @DBH@.
-close :: DBH -> IO ()
+-- * @DB@.
+close :: MonadIO m => DB -> m ()
 close dbh =
-  modifyMVar_
-    (dbhVar dbh)
-    (\mfptr -> do
-       maybe
-         (return ())
-         finalizeForeignPtr
-         mfptr
-       -- Previous line: The fptr would be finalized _eventually_, so
-       -- no memory leaks. But calling @close@ indicates you want to
-       -- release the resources right now.
-       --
-       -- Also, a foreign pointer's finalizer is ran and then deleted,
-       -- so you can't double-free.
-       pure Nothing)
+  liftIO
+    (modifyMVar_
+       (dbVar dbh)
+       (\mfptr -> do
+          maybe (return ()) finalizeForeignPtr mfptr
+          -- Previous line: The fptr would be finalized _eventually_, so
+          -- no memory leaks. But calling @close@ indicates you want to
+          -- release the resources right now.
+          --
+          -- Also, a foreign pointer's finalizer is ran and then deleted,
+          -- so you can't double-free.
+          pure Nothing))
+
+-- | Delete value at @key@.
+delete :: MonadIO m => DB -> WriteOptions -> ByteString -> m ()
+delete dbh writeOpts key =
+  liftIO
+    (withDBPtr
+       dbh
+       "delete"
+       (\dbPtr ->
+          withWriteOptions
+            writeOpts
+            (\opts_ptr ->
+               S.unsafeUseAsCStringLen
+                 key
+                 (\(key_ptr, klen) ->
+                    assertNotError
+                      "c_rocksdb_delete"
+                      (c_rocksdb_delete
+                         dbPtr
+                         opts_ptr
+                         key_ptr
+                         (fromIntegral klen))))))
 
 -- | Put a @value@ at @key@.
-put :: DBH -> ByteString -> ByteString -> IO ()
-put dbh key value =
-  withDBPtr
-    dbh
-    "put"
-    (\dbPtr ->
-       S.unsafeUseAsCStringLen
-         key
-         (\(key_ptr, klen) ->
-            S.unsafeUseAsCStringLen
-              value
-              (\(val_ptr, vlen) ->
-                 withWriteOptions
-                   (\optsPtr ->
-                      assertNotError
-                        "put"
-                        (c_rocksdb_put
-                           dbPtr
-                           optsPtr
-                           key_ptr
-                           (fromIntegral klen)
-                           val_ptr
-                           (fromIntegral vlen))))))
+put :: MonadIO m => DB -> WriteOptions -> ByteString -> ByteString -> m ()
+put dbh writeOpts key value =
+  liftIO
+    (withDBPtr
+       dbh
+       "put"
+       (\dbPtr ->
+          S.unsafeUseAsCStringLen
+            key
+            (\(key_ptr, klen) ->
+               S.unsafeUseAsCStringLen
+                 value
+                 (\(val_ptr, vlen) ->
+                    withWriteOptions
+                      writeOpts
+                      (\optsPtr ->
+                         assertNotError
+                           "put"
+                           (c_rocksdb_put
+                              dbPtr
+                              optsPtr
+                              key_ptr
+                              (fromIntegral klen)
+                              val_ptr
+                              (fromIntegral vlen)))))))
 
 -- | Get a value at @key@.
-get :: DBH -> ByteString -> IO (Maybe ByteString)
-get dbh key =
-  withDBPtr
-    dbh
-    "get"
-    (\dbPtr ->
-       S.unsafeUseAsCStringLen
-         key
-         (\(key_ptr, klen) ->
-            alloca
-              (\vlen_ptr ->
-                 withReadOptions
-                   (\optsPtr -> do
-                      val_ptr <-
-                        assertNotError
-                          "get"
-                          (c_rocksdb_get
-                             dbPtr
-                             optsPtr
-                             key_ptr
-                             (fromIntegral klen)
-                             vlen_ptr)
-                      vlen <- peek vlen_ptr
-                      if val_ptr == nullPtr
-                        then return Nothing
-                    -- Below: we as callers of the rocksDB C library
-                    -- own the malloc'd string and we are supposed to
-                    -- free it ourselves.
-                    --
-                    -- See facebook's example https://github.com/facebook/rocksdb/blob/master/examples/c_simple_example.c#L53
-                    --
-                    -- S.unsafeUseAsCStringLen re-uses with no copying
-                    -- the C array and adds a free() finalizer.
-                        else fmap
-                               Just
-                               (S.unsafePackMallocCStringLen
-                                  (val_ptr, fromIntegral vlen))))))
+get :: MonadIO m => DB -> ReadOptions -> ByteString -> m (Maybe ByteString)
+get dbh readOpts key =
+  liftIO
+    (withDBPtr
+       dbh
+       "get"
+       (\dbPtr ->
+          S.unsafeUseAsCStringLen
+            key
+            (\(key_ptr, klen) ->
+               alloca
+                 (\vlen_ptr ->
+                    withReadOptions
+                      readOpts
+                      (\optsPtr -> do
+                         val_ptr <-
+                           assertNotError
+                             "get"
+                             (c_rocksdb_get
+                                dbPtr
+                                optsPtr
+                                key_ptr
+                                (fromIntegral klen)
+                                vlen_ptr)
+                         vlen <- peek vlen_ptr
+                         if val_ptr == nullPtr
+                           then return Nothing
+                            -- Below: we as callers of the rocksDB C library
+                            -- own the malloc'd string and we are supposed to
+                            -- free it ourselves.
+                            --
+                            -- See facebook's example https://github.com/facebook/rocksdb/blob/master/examples/c_simple_example.c#L53
+                            --
+                            -- S.unsafeUseAsCStringLen re-uses with no copying
+                            -- the C array and adds a free() finalizer.
+                           else fmap
+                                  Just
+                                  (S.unsafePackMallocCStringLen
+                                     (val_ptr, fromIntegral vlen)))))))
+
+-- | Write a batch of operations atomically.
+write :: MonadIO m => DB -> WriteOptions -> [BatchOp] -> m ()
+write dbh opts batch =
+  liftIO
+    (withDBPtr
+       dbh
+       "write"
+       (\dbPtr ->
+          withWriteOptions
+            opts
+            (\writeOpts ->
+               withWriteBatch
+                 (\writerPtr -> do
+                    mapM_ (batchAdd writerPtr) batch
+                    assertNotError
+                      "c_rocksdb_write"
+                      (c_rocksdb_write dbPtr writeOpts writerPtr)
+                    -- Ensure @ByteString@s (and respective shared @CStringLen@s) aren't GC'ed
+                    -- until here.
+                    mapM_ touch batch))))
+  where
+    batchAdd batch_ptr (Put key val) =
+      S.unsafeUseAsCStringLen key $ \(key_ptr, klen) ->
+        S.unsafeUseAsCStringLen val $ \(val_ptr, vlen) ->
+          c_rocksdb_writebatch_put
+            batch_ptr
+            key_ptr
+            (fromIntegral klen)
+            val_ptr
+            (fromIntegral vlen)
+    batchAdd batch_ptr (Del key) =
+      S.unsafeUseAsCStringLen key $ \(key_ptr, klen) ->
+        c_rocksdb_writebatch_delete batch_ptr key_ptr (fromIntegral klen)
+    touch (Put (PS p _ _) (PS p' _ _)) = do
+      touchForeignPtr p
+      touchForeignPtr p'
+    touch (Del (PS p _ _)) = touchForeignPtr p
+
+createIter :: MonadIO m => DB -> ReadOptions -> m Iterator
+createIter = undefined
+
+releaseIter :: MonadIO m => Iterator -> m ()
+releaseIter = undefined
+
+iterSeek :: MonadIO m => Iterator -> ByteString -> m ()
+iterSeek = undefined
+
+iterEntry :: MonadIO m => Iterator -> m (Maybe (ByteString, ByteString))
+iterEntry = undefined
+
+iterNext :: MonadIO m => Iterator -> m ()
+iterNext = undefined
 
 --------------------------------------------------------------------------------
 -- Internal functions
 
 -- | Do something with the pointer inside. This is thread-safe.
-withDBPtr :: DBH -> String -> (Ptr DB -> IO a) -> IO a
+withDBPtr :: DB -> String -> (Ptr CDB -> IO a) -> IO a
 withDBPtr dbh label f =
   withMVar
-    (dbhVar dbh)
+    (dbVar dbh)
     (\mfptr ->
        case mfptr of
          Nothing -> throwIO (DatabaseIsClosed label)
          Just db -> withForeignPtr db f)
 
-withOptions :: (Ptr Options -> IO a) -> IO a
+withWriteBatch :: (Ptr CWriteBatch -> IO a) -> IO a
+withWriteBatch =
+  bracket
+    (assertNotNull "c_rocksdb_writebatch_create" c_rocksdb_writebatch_create)
+    c_rocksdb_writebatch_destroy
+
+withOptions :: (Ptr COptions -> IO a) -> IO a
 withOptions =
   bracket
     (assertNotNull "c_rocksdb_options_create" c_rocksdb_options_create)
     c_rocksdb_options_destroy
 
-withWriteOptions :: (Ptr WriteOptions -> IO a) -> IO a
-withWriteOptions =
+withWriteOptions :: WriteOptions -> (Ptr CWriteOptions -> IO a) -> IO a
+withWriteOptions WriteOptions =
   bracket
     (assertNotNull "c_rocksdb_writeoptions_create" c_rocksdb_writeoptions_create)
     c_rocksdb_writeoptions_destroy
 
-withReadOptions :: (Ptr ReadOptions -> IO a) -> IO a
-withReadOptions =
+withReadOptions :: ReadOptions -> (Ptr CReadOptions -> IO a) -> IO a
+withReadOptions ReadOptions =
   bracket
     (assertNotNull "c_rocksdb_readoptions_create" c_rocksdb_readoptions_create)
     c_rocksdb_readoptions_destroy
 
-replaceEncoding :: OpenConfig -> IO GHC.TextEncoding
+replaceEncoding :: Options -> IO GHC.TextEncoding
 #ifdef mingw32_HOST_OS
 replaceEncoding _ = GHC.getFileSystemEncoding
 #else
 replaceEncoding opts = do
   oldenc <- GHC.getFileSystemEncoding
-  when (openConfigCreateIfMissing opts) (GHC.setFileSystemEncoding GHC.utf8)
+  when (optionsCreateIfMissing opts) (GHC.setFileSystemEncoding GHC.utf8)
   pure oldenc
 #endif
 
@@ -274,50 +411,80 @@ assertNotError label f =
 --------------------------------------------------------------------------------
 -- Foreign (unsafe) bindings
 
-data Options
-data WriteOptions
-data ReadOptions
-data DB
+data COptions
+data CWriteOptions
+data CWriteBatch
+data CReadOptions
+data CDB
 
 foreign import ccall safe "rocksdb/c.h rocksdb_options_create"
-  c_rocksdb_options_create :: IO (Ptr Options)
+  c_rocksdb_options_create :: IO (Ptr COptions)
 
 foreign import ccall safe "rocksdb/c.h rocksdb_options_destroy"
-  c_rocksdb_options_destroy :: Ptr Options -> IO ()
+  c_rocksdb_options_destroy :: Ptr COptions -> IO ()
 
 foreign import ccall safe "rocksdb/c.h rocksdb_options_set_create_if_missing"
-  c_rocksdb_options_set_create_if_missing :: Ptr Options -> Bool -> IO ()
+  c_rocksdb_options_set_create_if_missing :: Ptr COptions -> Bool -> IO ()
 
 foreign import ccall safe "rocksdb/c.h rocksdb_open"
-  c_rocksdb_open :: Ptr Options -> CString -> Ptr CString -> IO (Ptr DB)
+  c_rocksdb_open :: Ptr COptions -> CString -> Ptr CString -> IO (Ptr CDB)
 
 foreign import ccall safe "rocksdb/c.h &rocksdb_close"
-  c_rocksdb_close_funptr :: FunPtr (Ptr DB -> IO ())
+  c_rocksdb_close_funptr :: FunPtr (Ptr CDB -> IO ())
 
 foreign import ccall safe "rocksdb/c.h rocksdb_put"
-  c_rocksdb_put :: Ptr DB
-                -> Ptr WriteOptions
+  c_rocksdb_put :: Ptr CDB
+                -> Ptr CWriteOptions
                 -> CString -> CSize
                 -> CString -> CSize
                 -> Ptr CString
                 -> IO ()
 
 foreign import ccall safe "rocksdb/c.h rocksdb_get"
-  c_rocksdb_get :: Ptr DB
-                -> Ptr ReadOptions
+  c_rocksdb_get :: Ptr CDB
+                -> Ptr CReadOptions
                 -> CString -> CSize
                 -> Ptr CSize -- ^ Output length.
                 -> Ptr CString
                 -> IO CString
 
 foreign import ccall safe "rocksdb/c.h rocksdb_writeoptions_create"
-  c_rocksdb_writeoptions_create :: IO (Ptr WriteOptions)
+  c_rocksdb_writeoptions_create :: IO (Ptr CWriteOptions)
 
 foreign import ccall safe "rocksdb/c.h rocksdb_writeoptions_destroy"
-  c_rocksdb_writeoptions_destroy :: Ptr WriteOptions -> IO ()
+  c_rocksdb_writeoptions_destroy :: Ptr CWriteOptions -> IO ()
 
 foreign import ccall safe "rocksdb/c.h rocksdb_readoptions_create"
-  c_rocksdb_readoptions_create :: IO (Ptr ReadOptions)
+  c_rocksdb_readoptions_create :: IO (Ptr CReadOptions)
 
 foreign import ccall safe "rocksdb/c.h rocksdb_readoptions_destroy"
-  c_rocksdb_readoptions_destroy :: Ptr ReadOptions -> IO ()
+  c_rocksdb_readoptions_destroy :: Ptr CReadOptions -> IO ()
+
+foreign import ccall safe "rocksdb/c.h rocksdb_writebatch_create"
+  c_rocksdb_writebatch_create :: IO (Ptr CWriteBatch)
+
+foreign import ccall safe "rocksdb/c.h rocksdb_writebatch_destroy"
+  c_rocksdb_writebatch_destroy :: Ptr CWriteBatch -> IO ()
+
+foreign import ccall safe "rocksdb/c.h rocksdb_writebatch_put"
+  c_rocksdb_writebatch_put :: Ptr CWriteBatch
+                           -> CString -> CSize
+                           -> CString -> CSize
+                           -> IO ()
+
+foreign import ccall safe "rocksdb/c.h rocksdb_writebatch_delete"
+  c_rocksdb_writebatch_delete :: Ptr CWriteBatch -> CString -> CSize -> IO ()
+
+foreign import ccall safe "rocksdb/c.h rocksdb_write"
+  c_rocksdb_write :: Ptr CDB
+                  -> Ptr CWriteOptions
+                  -> Ptr CWriteBatch
+                  -> Ptr CString
+                  -> IO ()
+
+foreign import ccall safe "rocksdb/c.h rocksdb_delete"
+  c_rocksdb_delete :: Ptr CDB
+                   -> Ptr CWriteOptions
+                   -> CString -> CSize
+                   -> Ptr CString
+                   -> IO ()
